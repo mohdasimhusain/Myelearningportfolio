c7e59ee604eb783d9b02317c01bab7db9a24eea1
mohdasimhusain <elearndubai@gmail.com>
2021-08-31 13:54:13 +0400

    My Projects

---
+++ Business Template/story_content/user.js
@@ -0,0 +1,583 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5lorWGnaYOb":
+        Script1();
+        break;
+      case "5gfB4nWkTt8":
+        Script2();
+        break;
+      case "6o6EEP1tuLa":
+        Script3();
+        break;
+      case "5pbp2ySO7zf":
+        Script4();
+        break;
+      case "6PltPbxWXlP":
+        Script5();
+        break;
+      case "5uYG7w2Z13m":
+        Script6();
+        break;
+      case "6CmXTEbPZq1":
+        Script7();
+        break;
+      case "6h0AarrqTTg":
+        Script8();
+        break;
+      case "6MLz1A3cHXj":
+        Script9();
+        break;
+      case "65Ll0Shkekl":
+        Script10();
+        break;
+      case "6cIEQJFnNMH":
+        Script11();
+        break;
+      case "6kwicbRmPT8":
+        Script12();
+        break;
+      case "6R5vphCxmc2":
+        Script13();
+        break;
+      case "6nzbdWmLS7r":
+        Script14();
+        break;
+      case "6nbOHddMsCL":
+        Script15();
+        break;
+      case "6ondvIoeMDi":
+        Script16();
+        break;
+      case "6gQOfhJVowA":
+        Script17();
+        break;
+      case "6UNQCMW4m5r":
+        Script18();
+        break;
+      case "6E8A6HuRPOE":
+        Script19();
+        break;
+      case "6b8ETZpbKLm":
+        Script20();
+        break;
+      case "5Wu1P7aqtBn":
+        Script21();
+        break;
+      case "5fNdUDf1rBt":
+        Script22();
+        break;
+      case "5Z9da1PRUr4":
+        Script23();
+        break;
+      case "6kuoqkxjtxc":
+        Script24();
+        break;
+      case "6BNChJFGI9X":
+        Script25();
+        break;
+      case "6ELBgunR8sq":
+        Script26();
+        break;
+      case "6KQFV3jiW7Y":
+        Script27();
+        break;
+      case "64Y1KUWLXr8":
+        Script28();
+        break;
+      case "5sjOhD3Ootl":
+        Script29();
+        break;
+      case "6LGJDvoV1Xi":
+        Script30();
+        break;
+      case "6Mg9U7GACtg":
+        Script31();
+        break;
+      case "67NuedvHE8k":
+        Script32();
+        break;
+      case "6YUYY3VexYQ":
+        Script33();
+        break;
+      case "6mFvBEUQt9M":
+        Script34();
+        break;
+      case "6T8dOKjbOEX":
+        Script35();
+        break;
+      case "5WZdhoxDpIr":
+        Script36();
+        break;
+      case "6CDAz20Fvhf":
+        Script37();
+        break;
+      case "66hHys7gHNJ":
+        Script38();
+        break;
+      case "5XAu4s4aF3h":
+        Script39();
+        break;
+      case "6IGXVaEExv1":
+        Script40();
+        break;
+      case "5ooMSWOCZsT":
+        Script41();
+        break;
+      case "696p42olrL8":
+        Script42();
+        break;
+      case "6IJZjTnhhhI":
+        Script43();
+        break;
+      case "65Kl0BAwVkE":
+        Script44();
+        break;
+      case "6R7edDezNIp":
+        Script45();
+        break;
+      case "5bqs1oAPSF3":
+        Script46();
+        break;
+      case "6bOtLILYjt4":
+        Script47();
+        break;
+      case "6dqluxdeIoY":
+        Script48();
+        break;
+      case "6oU1dwD88yO":
+        Script49();
+        break;
+      case "5dQwV0ZLhQ1":
+        Script50();
+        break;
+      case "6eGmyLanEpv":
+        Script51();
+        break;
+      case "6cvnJ1mLjq7":
+        Script52();
+        break;
+      case "6L8dO5QnGzV":
+        Script53();
+        break;
+      case "6gsdDbWR34y":
+        Script54();
+        break;
+      case "68ftADNUv9S":
+        Script55();
+        break;
+      case "6Pq9XNe3E2S":
+        Script56();
+        break;
+      case "6RHn4mJ7ek7":
+        Script57();
+        break;
+      case "5iaQMxgsP4M":
+        Script58();
+        break;
+      case "6kXafWdiypG":
+        Script59();
+        break;
+      case "5YglfgzCLL4":
+        Script60();
+        break;
+      case "5aS1FZNzo2e":
+        Script61();
+        break;
+      case "6nNWY9S4nOf":
+        Script62();
+        break;
+      case "6HTGOyRp4Cp":
+        Script63();
+        break;
+      case "5rZowUvosQc":
+        Script64();
+        break;
+      case "6WcczvIVkRe":
+        Script65();
+        break;
+      case "5dxzCBJKkSV":
+        Script66();
+        break;
+      case "5Z8aC9hJP2f":
+        Script67();
+        break;
+      case "6rTA98FlBKr":
+        Script68();
+        break;
+      case "6ZXg0dy3Vzd":
+        Script69();
+        break;
+      case "6PRG2shHuHM":
+        Script70();
+        break;
+      case "5cme6n6vqex":
+        Script71();
+        break;
+      case "6iuSP7hWtno":
+        Script72();
+        break;
+  }
+}
+
+function Script1()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script2()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script3()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script4()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script5()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script6()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script7()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script8()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script9()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script10()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script11()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script12()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script13()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script14()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script15()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script16()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script17()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script18()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script19()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script20()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script21()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script22()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script23()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script24()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script25()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script26()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script27()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script28()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script29()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script30()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script31()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script32()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script33()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script34()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script35()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script36()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script37()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script38()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script39()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script40()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script41()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script42()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script43()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script44()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script45()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script46()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script47()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script48()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script49()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script50()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script51()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script52()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script53()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script54()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script55()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script56()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script57()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script58()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script59()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script60()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script61()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script62()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script63()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script64()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script65()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script66()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script67()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script68()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script69()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script70()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script71()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
+function Script72()
+{
+  parent.document.body.style.backgroundColor = "#1F172F";
+}
+
---
+++ Red Cross Australia/story_content/user.js
@@ -0,0 +1,273 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6jhKbu3ND2h":
+        Script1();
+        break;
+      case "6Lz76mAJbU7":
+        Script2();
+        break;
+      case "6FvBft68zJP":
+        Script3();
+        break;
+      case "5z2zOBN56fB":
+        Script4();
+        break;
+      case "5xZUKzpzVag":
+        Script5();
+        break;
+      case "5jKuSESy3Fq":
+        Script6();
+        break;
+      case "6LG3hBxVDK8":
+        Script7();
+        break;
+      case "6rjsXPxyAzE":
+        Script8();
+        break;
+      case "6De3caYG3At":
+        Script9();
+        break;
+      case "5hyTLoX9HCZ":
+        Script10();
+        break;
+      case "6i9RLCD0cj8":
+        Script11();
+        break;
+      case "5uMURiFJc23":
+        Script12();
+        break;
+      case "6maH0RE8g20":
+        Script13();
+        break;
+      case "5W3zO1rfWHY":
+        Script14();
+        break;
+  }
+}
+
+function Script1()
+{
+  var player = GetPlayer();
+
+function findLMSAPI(win) {
+     if (win.hasOwnProperty("GetStudentID")) return win;
+     else if (win.parent == win) return null;
+     else return findLMSAPI(win.parent);
+}
+var lmsAPI = findLMSAPI(this);
+
+var lmsStatus = lmsAPI.GetStatus();
+player.SetVar("lmsStatus", lmsStatus);
+lmsAPI.SetStatus(lmsStatus);
+}
+
+function Script2()
+{
+  var player = GetPlayer();
+
+function findLMSAPI(win) {
+     if (win.hasOwnProperty("GetStudentID")) return win;
+     else if (win.parent == win) return null;
+     else return findLMSAPI(win.parent);
+}
+var lmsAPI = findLMSAPI(this);
+
+var lmsStatus = lmsAPI.GetStatus();
+player.SetVar("lmsStatus", lmsStatus);
+lmsAPI.SetStatus(lmsStatus);
+}
+
+function Script3()
+{
+  var player = GetPlayer();
+
+function findLMSAPI(win) {
+     if (win.hasOwnProperty("GetStudentID")) return win;
+     else if (win.parent == win) return null;
+     else return findLMSAPI(win.parent);
+}
+var lmsAPI = findLMSAPI(this);
+
+var lmsStatus = lmsAPI.GetStatus();
+player.SetVar("lmsStatus", lmsStatus);
+lmsAPI.SetStatus(lmsStatus);
+}
+
+function Script4()
+{
+  var player = GetPlayer();
+
+function findLMSAPI(win) {
+     if (win.hasOwnProperty("GetStudentID")) return win;
+     else if (win.parent == win) return null;
+     else return findLMSAPI(win.parent);
+}
+var lmsAPI = findLMSAPI(this);
+
+var lmsStatus = lmsAPI.GetStatus();
+player.SetVar("lmsStatus", lmsStatus);
+lmsAPI.SetStatus(lmsStatus);
+}
+
+function Script5()
+{
+  var player = GetPlayer();
+
+function findLMSAPI(win) {
+     if (win.hasOwnProperty("GetStudentID")) return win;
+     else if (win.parent == win) return null;
+     else return findLMSAPI(win.parent);
+}
+var lmsAPI = findLMSAPI(this);
+
+var lmsStatus = lmsAPI.GetStatus();
+player.SetVar("lmsStatus", lmsStatus);
+lmsAPI.SetStatus(lmsStatus);
+}
+
+function Script6()
+{
+  var player = GetPlayer();
+
+function findLMSAPI(win) {
+     if (win.hasOwnProperty("GetStudentID")) return win;
+     else if (win.parent == win) return null;
+     else return findLMSAPI(win.parent);
+}
+var lmsAPI = findLMSAPI(this);
+
+var lmsStatus = lmsAPI.GetStatus();
+player.SetVar("lmsStatus", lmsStatus);
+lmsAPI.SetStatus(lmsStatus);
+}
+
+function Script7()
+{
+  var player = GetPlayer();
+
+function findLMSAPI(win) {
+     if (win.hasOwnProperty("GetStudentID")) return win;
+     else if (win.parent == win) return null;
+     else return findLMSAPI(win.parent);
+}
+var lmsAPI = findLMSAPI(this);
+
+var lmsStatus = lmsAPI.GetStatus();
+player.SetVar("lmsStatus", lmsStatus);
+lmsAPI.SetStatus(lmsStatus);
+}
+
+function Script8()
+{
+  var player = GetPlayer();
+
+function findLMSAPI(win) {
+     if (win.hasOwnProperty("GetStudentID")) return win;
+     else if (win.parent == win) return null;
+     else return findLMSAPI(win.parent);
+}
+var lmsAPI = findLMSAPI(this);
+
+var lmsStatus = lmsAPI.GetStatus();
+player.SetVar("lmsStatus", lmsStatus);
+lmsAPI.SetStatus(lmsStatus);
+}
+
+function Script9()
+{
+  var player = GetPlayer();
+
+function findLMSAPI(win) {
+     if (win.hasOwnProperty("GetStudentID")) return win;
+     else if (win.parent == win) return null;
+     else return findLMSAPI(win.parent);
+}
+var lmsAPI = findLMSAPI(this);
+
+var lmsStatus = lmsAPI.GetStatus();
+player.SetVar("lmsStatus", lmsStatus);
+lmsAPI.SetStatus(lmsStatus);
+}
+
+function Script10()
+{
+  var player = GetPlayer();
+
+function findLMSAPI(win) {
+     if (win.hasOwnProperty("GetStudentID")) return win;
+     else if (win.parent == win) return null;
+     else return findLMSAPI(win.parent);
+}
+var lmsAPI = findLMSAPI(this);
+
+var lmsStatus = lmsAPI.GetStatus();
+player.SetVar("lmsStatus", lmsStatus);
+lmsAPI.SetStatus(lmsStatus);
+}
+
+function Script11()
+{
+  var player = GetPlayer();
+
+function findLMSAPI(win) {
+     if (win.hasOwnProperty("GetStudentID")) return win;
+     else if (win.parent == win) return null;
+     else return findLMSAPI(win.parent);
+}
+var lmsAPI = findLMSAPI(this);
+
+var lmsStatus = lmsAPI.GetStatus();
+player.SetVar("lmsStatus", lmsStatus);
+lmsAPI.SetStatus(lmsStatus);
+}
+
+function Script12()
+{
+  var player = GetPlayer();
+
+function findLMSAPI(win) {
+     if (win.hasOwnProperty("GetStudentID")) return win;
+     else if (win.parent == win) return null;
+     else return findLMSAPI(win.parent);
+}
+var lmsAPI = findLMSAPI(this);
+
+var lmsStatus = lmsAPI.GetStatus();
+player.SetVar("lmsStatus", lmsStatus);
+lmsAPI.SetStatus(lmsStatus);
+}
+
+function Script13()
+{
+  var player = GetPlayer();
+
+function findLMSAPI(win) {
+     if (win.hasOwnProperty("GetStudentID")) return win;
+     else if (win.parent == win) return null;
+     else return findLMSAPI(win.parent);
+}
+var lmsAPI = findLMSAPI(this);
+
+var lmsStatus = lmsAPI.GetStatus();
+player.SetVar("lmsStatus", lmsStatus);
+lmsAPI.SetStatus(lmsStatus);
+}
+
+function Script14()
+{
+  var player = GetPlayer();
+
+function findLMSAPI(win) {
+     if (win.hasOwnProperty("GetStudentID")) return win;
+     else if (win.parent == win) return null;
+     else return findLMSAPI(win.parent);
+}
+var lmsAPI = findLMSAPI(this);
+
+var lmsStatus = lmsAPI.GetStatus();
+player.SetVar("lmsStatus", lmsStatus);
+lmsAPI.SetStatus(lmsStatus);
+}
+
